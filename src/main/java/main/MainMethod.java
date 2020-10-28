f0c53375d36432046e862c0b572127e30fcad27b
almir james <lucenaalmirjames@gmail.com>
2020-10-28 17:29:12 +0800

    your token here

--- src/main/java/main/MainMethod.java
+++ src/main/java/main/MainMethod.java
@@ -26,7 +26,7 @@ public class MainMethod {
 			intents.add(GatewayIntent.GUILD_VOICE_STATES);
 			intents.add(GatewayIntent.GUILD_EMOJIS);
 
-		JDA jda = JDABuilder.createDefault("NzU3NTE3MzcwNjE2MTg0ODgz.X2hjBw._ZAKgLoscmCNz4JC6LCAdCHChvw", intents)
+		JDA jda = JDABuilder.createDefault("YOUR_TOKEN_HERE", intents)
 				.setStatus(OnlineStatus.DO_NOT_DISTURB).build();
 
 
