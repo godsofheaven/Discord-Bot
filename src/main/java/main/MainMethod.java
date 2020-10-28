eb86cc07ce3200ef28dcb09098dec160c383a77c
shadowprend <jandranrebbalangue@gmail.com>
2020-10-28 09:22:24 +0800

    add new commands

--- src/main/java/main/MainMethod.java
+++ src/main/java/main/MainMethod.java
@@ -2,7 +2,7 @@ package main;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
+
 import javax.security.auth.login.LoginException;
 import botCommands.BotInfoCommand;
 import botCommands.botMemberVoiceCommands.DisconnectMember;
@@ -11,9 +11,9 @@ import botCommands.botMemberVoiceCommands.MoveMember;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
-import net.dv8tion.jda.api.entities.VoiceChannel;
+
 import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.dv8tion.jda.api.utils.cache.SnowflakeCacheView;
+
 
 public class MainMethod {
 
