723ce8bec3bbb045cdd3c86fa29c2ac80e3254a2
almir james <lucenaalmirjames@gmail.com>
2020-10-29 19:29:43 +0800

    not yet done

---
+++ src/test/java/youtubeTest/YoutubeSearchTrackTest.java
@@ -0,0 +1,20 @@
+package youtubeTest;
+
+import lavaPlayer.YoutubeSearch;
+import org.junit.Test;
+import org.mockito.Mock;
+
+public class YoutubeSearchTrackTest {
+
+        private void setup () {
+            YoutubeSearch youtubeSearchAudio = new YoutubeSearch();
+        }
+
+
+
+        @Test
+        public void youtubeTest () {
+             
+        }
+
+}
